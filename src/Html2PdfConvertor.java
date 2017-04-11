2f56c5288ce66f2acf61b4dae2d775d95b580960
bharathkumar.kandaswamy1@t-mobile.com <bharathkumar.kandaswamy1@t-mobile.com>
2017-04-11 11:14:17 -0700

    Signed-off-by: bharathkumar.kandaswamy1@t-mobile.com <bharathkumar.kandaswamy1@t-mobile.com>

--- src/Html2PdfConvertor.java
+++ src/Html2PdfConvertor.java
@@ -1,6 +1,8 @@
+import java.io.BufferedReader;
 import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.InputStream;
 import java.io.OutputStream;
 
@@ -12,12 +14,28 @@ public class Html2PdfConvertor {
 
 	public static void main(String[] args) {
 		try {
-			String k = "<html><body> This is my Project </body></html>";
+			File htmlFile = new File(
+					"C:\\Work\\SOURCES\\TMOSan\\eservice-mytmo_uitest\\eservice_mytmo_tests\\test-output\\Extent.html");
+			BufferedReader br = null;
+			FileReader fr = null;
+			String completeHTML = "";
+			try {
+				fr = new FileReader(htmlFile);
+				br = new BufferedReader(fr);
+				String sCurrentLine;
+				while ((sCurrentLine = br.readLine()) != null) {
+					completeHTML += sCurrentLine;
+				}
+			} catch (Exception ex) {
+				ex.printStackTrace();
+			} finally {
+				System.out.println("HTML Read :::: " + completeHTML);
+			}
 			OutputStream file = new FileOutputStream(new File("C:\\users\\prokarma\\Test.pdf"));
 			Document document = new Document();
 			PdfWriter writer = PdfWriter.getInstance(document, file);
 			document.open();
-			InputStream is = new ByteArrayInputStream(k.getBytes());
+			InputStream is = new ByteArrayInputStream(completeHTML.getBytes());
 			XMLWorkerHelper.getInstance().parseXHtml(writer, document, is);
 			document.close();
 			file.close();
